39adc3deb2c94330d346940c7ca8e8b0340ce58c
romyluna <134311514+romyluna@users.noreply.github.com>
2025-08-05 15:41:51 -0300

    MODIFY (Security): modifique de nuevo porque no estaba funcionando correctamente ahora todo lo que esta con la url factura necesita login

--- src/main/java/com/portafolio/facturacion/configuracion/Security.java
+++ src/main/java/com/portafolio/facturacion/configuracion/Security.java
@@ -16,7 +16,10 @@ public class Security {
         ).authorizeHttpRequests(
                 auth->{
                     //todas las uris que comiencen con esta ruta /api/V1/productos no necesitan autorizacion de acceso
-                    auth.requestMatchers("/api/V1/productos/**").permitAll()
+                    auth.requestMatchers("/api/V1/productos/**","/swagger-ui/**","/swagger-ui.html",
+                                    "/v3/api-docs/**",
+                                    "/swagger-resources/**",
+                                    "/webjars/**").permitAll()
                             .anyRequest().authenticated();//el resto tiene que estar autenticado
                     //auth.anyRequest().authenticated(); // todas las rutas o endpoints de la aplicación requieren que el usuario esté autenticado.
                 }
